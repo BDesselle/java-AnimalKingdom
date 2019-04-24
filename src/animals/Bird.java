2b8b06de4f6555d70ef0978f48d35cfbe42a762f
Brandon Desselle <desselle.brandon2@gmail.com>
2019-04-24 17:24:42 -0500

    Create Bird.java

---
+++ src/animals/Bird.java
@@ -0,0 +1,29 @@
+package animals;
+
+public class Bird extends AnimalImpl {
+
+    public Bird(String name, int yearDiscovered) {
+        super(name, yearDiscovered);
+    }
+
+    @Override
+    public String move() {
+        return "fly";
+    }
+
+    @Override
+    public String breath() {
+        return "lungs";
+    }
+
+    @Override
+    public String reproduce() {
+        return "eggs";
+    }
+
+    @Override
+    public double eat(double amount) {
+        return 0;
+    }
+
+}
