f3f08a7747464d3519977b79cf87392ac6bf4680
Brandon Desselle <desselle.brandon2@gmail.com>
2019-04-24 17:24:51 -0500

    Create Mammal.java

---
+++ src/animals/Mammal.java
@@ -0,0 +1,28 @@
+package animals;
+
+public class Mammal extends AnimalImpl {
+
+    public Mammal(String name, int yearDiscovered) {
+        super(name, yearDiscovered);
+    }
+
+    @Override
+    public String move() {
+        return "walk";
+    }
+
+    @Override
+    public String breath() {
+        return "lungs";
+    }
+
+    @Override
+    public String reproduce() {
+        return "live births";
+    }
+
+    @Override
+    public double eat(double amount) {
+        return 0.0;
+    }
+}
