9c2705a9c0e28869823c19dd945ad4957676c0b9
Brandon Desselle <desselle.brandon2@gmail.com>
2019-04-24 17:24:45 -0500

    Create Fish.java

---
+++ src/animals/Fish.java
@@ -0,0 +1,29 @@
+package animals;
+
+public class Fish extends AnimalImpl {
+
+  public Fish(String name, int yearDiscovered) {
+    super(name, yearDiscovered);
+  }
+
+  @Override
+  public String move() {
+    return "swim";
+  }
+
+  @Override
+  public String breath() {
+    return "gills";
+  }
+
+  @Override
+  public String reproduce() {
+    return "eggs";
+  }
+
+  @Override
+  public double eat(double amount) {
+    return 0;
+  }
+
+}
