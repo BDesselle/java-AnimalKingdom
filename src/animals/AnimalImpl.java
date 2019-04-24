07ee4a1a1bb6e90eb530a3ec353869fa55857e29
Brandon Desselle <desselle.brandon2@gmail.com>
2019-04-24 17:24:39 -0500

    Create AnimalImpl.java

---
+++ src/animals/AnimalImpl.java
@@ -0,0 +1,55 @@
+package animals;
+
+public abstract class AnimalImpl implements Animal {
+    static int maxId = 0;
+    private int id;
+    private String name;
+    private int yearDiscovered;
+
+    public AnimalImpl(String name, int yearDiscovered) {
+        maxId++;
+        this.id = maxId;
+        this.name = name;
+        this.yearDiscovered = yearDiscovered;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYearDiscovered() {
+        return yearDiscovered;
+    }
+
+    public void setYearDiscovered(int yearDiscovered) {
+        this.yearDiscovered = yearDiscovered;
+    }
+
+    @Override
+    public String move() {
+        return null;
+    }
+
+    @Override
+    public String breath() {
+        return null;
+    }
+
+    @Override
+    public String reproduce() {
+        return null;
+    }
+
+    @Override
+    public double eat(double amount) {
+        return 0;
+    }
+}
